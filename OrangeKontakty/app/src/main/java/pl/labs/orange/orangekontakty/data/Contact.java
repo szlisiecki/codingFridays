bf7ff8ec0b28b49e57e4a259c7be5cd10f891351
Marcin.Mazurek <marcin.mazurek@likims.com>
2020-06-05 08:52:29 +0200

    add contact

---
+++ OrangeKontakty/app/src/main/java/pl/labs/orange/orangekontakty/data/Contact.java
@@ -0,0 +1,44 @@
+package pl.labs.orange.orangekontakty.data;
+
+public class Contact {
+
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
