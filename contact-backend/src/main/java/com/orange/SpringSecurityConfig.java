c2356b0caa08bc5d511b89a334ff1a5896c16188
Szymon Lisiecki <szymon.lisiecki@likims.com>
2020-11-13 16:14:49 +0100

    jwt

--- contact-backend/src/main/java/com/orange/SpringSecurityConfig.java
+++ contact-backend/src/main/java/com/orange/SpringSecurityConfig.java
@@ -1,14 +1,31 @@
 package com.orange;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	private RestAuthenticationSuccessHandler authenticationSuccessHandler;
+	private RestAuthenticationFailureHandler authenticationFailureHandler;
+	private String secret;
+
+	public SpringSecurityConfig(RestAuthenticationSuccessHandler authenticationSuccessHandler,
+            RestAuthenticationFailureHandler authenticationFailureHandler,
+            @Value("${jwt.secret}") String secret) {
+		this.authenticationSuccessHandler = authenticationSuccessHandler;
+		this.authenticationFailureHandler = authenticationFailureHandler;
+		this.secret = secret;
+}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("Szymon").password("{noop}Szymon").roles("MODIFIER","READER").and()
@@ -16,11 +33,36 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and()
-			.authorizeRequests()
-				.antMatchers(HttpMethod.GET, "/contacts/**"). hasRole("READER")
-				.antMatchers("/contacts/**").hasRole("MODIFIER").and()
-					.csrf().disable();
-	}
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(authenticationFilter())
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), super.userDetailsService(), secret))
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    }
+	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.httpBasic().and()
+//			.authorizeRequests()
+//				.antMatchers(HttpMethod.GET, "/contacts/**"). hasRole("READER")
+//				.antMatchers("/contacts/**").hasRole("MODIFIER").and()
+//					.csrf().disable();
+//	}
+	
+	@Bean
+    public JsonObjectAuthenticationFilter authenticationFilter() throws Exception {
+        JsonObjectAuthenticationFilter filter = new JsonObjectAuthenticationFilter();
+        filter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+        filter.setAuthenticationFailureHandler(authenticationFailureHandler);
+        filter.setAuthenticationManager(super.authenticationManager());
+        return filter;
+    }
 }
