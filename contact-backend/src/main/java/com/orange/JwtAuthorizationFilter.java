faf7f1f4dde0a6ed18972b7bc42635ba2ceca916
Szymon Lisiecki <szymon.lisiecki@likims.com>
2020-11-27 14:15:44 +0100

    create and verify token with roles

--- contact-backend/src/main/java/com/orange/JwtAuthorizationFilter.java
+++ contact-backend/src/main/java/com/orange/JwtAuthorizationFilter.java
@@ -2,8 +2,13 @@ package com.orange;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +20,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
@@ -45,13 +52,17 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
         String token = request.getHeader(TOKEN_HEADER);
         if (token != null && token.startsWith(TOKEN_PREFIX)) {
-            String userName = JWT.require(Algorithm.HMAC256(secret))
+            DecodedJWT verifiedToken = JWT.require(Algorithm.HMAC256(secret))
                     .build()
-                    .verify(token.replace(TOKEN_PREFIX, ""))
-                    .getSubject();
-            if (userName != null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
-                return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
+                    .verify(token.replace(TOKEN_PREFIX, ""));
+                    
+            if (verifiedToken.getSubject() != null) {
+//                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+//                userDetails.getAuthorities()
+            	List<String> roles = verifiedToken.getClaim("roles").asList(String.class);
+            	List<SimpleGrantedAuthority> rolesList = roles.stream().map(r -> new SimpleGrantedAuthority(r)).collect(Collectors.toList());
+            	
+                return new UsernamePasswordAuthenticationToken(verifiedToken.getSubject(), null, rolesList);
             }
         }
         return null;
--- contact-backend/src/main/java/com/orange/RestAuthenticationSuccessHandler.java
+++ contact-backend/src/main/java/com/orange/RestAuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -30,9 +34,21 @@ public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         UserDetails principal = (UserDetails) authentication.getPrincipal();
+        
+        
+        
+        for (GrantedAuthority grantedAuthority : principal.getAuthorities()) {
+        	grantedAuthority.getAuthority();
+		}
+        
+        // TODO refactor
+        List<String> collect = principal.getAuthorities().stream().map(a-> a.getAuthority()).collect(Collectors.toList());
+        String[] array = collect.toArray(new String[collect.size()]);
+        
         String token = JWT.create()
                 .withSubject(principal.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .withArrayClaim("roles", array)
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))              
                 .sign(Algorithm.HMAC256(secret));
         response.getOutputStream().print("{\"token\": \"" + token + "\"}");
     }
